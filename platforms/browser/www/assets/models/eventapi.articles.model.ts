1ce0f5f909a8dca5a8c68be9da584d869a034fdf
Ken Sodemann <ken@ionic.io>
2018-04-06 20:00:46 -0500

    chore(build): ignore build artifacts

--- platforms/browser/www/assets/models/eventapi.articles.model.ts
+++
@@ -1,9 +0,0 @@
-export class EventApiArticle {
-    author: string;
-    description: string;
-    title: string;
-    url: string;
-    urlToImage: string;
-    publishedAt: string;
-    
-}
--- plugins/cordova-plugin-ionic/src/ios/IonicCordovaCommon.h
+++
@@ -1,7 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-
-@interface IonicCordovaCommon : CDVPlugin
-
-- (void)getAppInfo:(CDVInvokedUrlCommand*)command;
-
-@end
