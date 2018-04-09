6ae6fa086fb6b1e0c7461fc0280d29a1ccc02b42
robroy92 <boulangerromain@gmail.com>
2018-04-09 22:15:17 +0200

    master

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -15,6 +15,11 @@ export class ProfilePage {
   id: number = 1;
   user_height: number = 0;
   user_hips: number = 0;
+  user_chest : number = 0;
+  user_waist : number = 0;
+  user_shoe : number = 0;
+  user_haircolor : string = '';
+  user_eyecolor : string = '';
   user_profile: any = [];
 
   constructor(
@@ -41,8 +46,16 @@ export class ProfilePage {
       this.user_profile = profile;
       console.log(this.user_profile);
       this.user_height = this.user_profile.user_height; 
+      this.user_chest = this.user_profile.user_chest; 
+      this.user_waist = this.user_profile.user_waist; 
       this.user_hips = this.user_profile.user_hips; 
+      this.user_shoe = this.user_profile.user_shoe; 
+      this.user_haircolor = this.user_profile.user_haircolor; 
+      this.user_eyecolor = this.user_profile.user_eyecolor; 
+      this.user_height = this.user_profile.user_height; 
+
       console.log(this.user_height);
+
     }
   );
  
